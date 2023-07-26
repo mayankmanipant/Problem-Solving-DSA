149f7dd3798165218a54083d095165899106c280
Mayank Mani Pant <102402423+mayankmanipant@users.noreply.github.com>
2023-07-26 12:07:25 +0530

    Create 1870_MinimumSpeedToArriveOnTime.cpp

---
+++ 1870_MinimumSpeedToArriveOnTime.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+double timeRequired(vector<int>&dist,int speed){
+    double time=0.0;
+    for(int i=0;i<dist.size();i++){
+        double t=(double)dist[i]/(double)speed;
+        time+=(i==dist.size()-1 ?t:ceil(t));
+    }
+    return time;
+}
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int left=1;
+        int right=1e7;
+        int minspeed=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(timeRequired(dist,mid)<=hour){
+                minspeed=mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return minspeed;
+    }
+};
