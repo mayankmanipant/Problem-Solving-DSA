6626799b80e63e3a3b84b4f735e42dc2b3f11b30
Mayank Mani Pant <102402423+mayankmanipant@users.noreply.github.com>
2024-02-19 13:56:40 +0530

    Create LC_904_Fruit ntoBaskets.cpp

---
+++ LC_904_Fruit ntoBaskets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        int i=0,j=0,ans=0;
+        unordered_map<int,int>mp;
+
+        while(j<n){
+            mp[fruits[j]]++;
+            
+            if(mp.size()<=2){
+                ans=max(ans,j-i+1);
+                j++;
+            }
+            else if(mp.size()>2){
+                while(mp.size()>2){
+                    mp[fruits[i]]--;
+                    if(mp[fruits[i]]==0){
+                        mp.erase(fruits[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        return ans;
+    }
+};
