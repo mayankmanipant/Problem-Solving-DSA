35fb014799187191d116f424a5d7770752c764b4
Mayank Mani Pant <102402423+mayankmanipant@users.noreply.github.com>
2025-08-29 17:09:20 +0530

    GFG Nth Fibonacci Number.cpp

---
+++ GFG Nth Fibonacci Number.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+class Solution {
+  public:
+    int f(int n,vector<int>& dp){
+     if(n<=1) return n;
+    
+        if(dp[n]!=-1) return dp[n];
+    
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    } 
+    int nthFibonacci(int n) {
+        // code here
+        vector<int>dp(n+1, -1);
+        return f(n,dp);
+    }
+};
